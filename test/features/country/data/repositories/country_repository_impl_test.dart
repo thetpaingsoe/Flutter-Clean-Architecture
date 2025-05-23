5ab7a4a039ed856ad60523c234d2b2eb47f1a4ec
Thet Paing Soe <thetpaingsoe.ptn@gmail.com>
2025-05-23 17:42:01 +0700

    Added Unit Test of Repo and Use Cases

---
+++ test/features/country/data/repositories/country_repository_impl_test.dart
@@ -0,0 +1,38 @@
+import 'package:flutter_clean_architecture/features/country/data/datasources/country_data_source.dart';
+import 'package:flutter_clean_architecture/features/country/data/repositories/country_repository_impl.dart';
+import 'package:flutter_clean_architecture/features/country/domain/entities/country.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'country_repository_impl_test.mocks.dart';
+
+@GenerateMocks([CountryDataSource])
+void main() {
+  late CountryRepositoryImpl repository;
+  late MockCountryDataSource mockDataSource;
+
+  setUp(() {
+    mockDataSource = MockCountryDataSource();
+    repository = CountryRepositoryImpl(countryDataSource: mockDataSource);
+  });
+
+  final testCountries = [
+    Country(name: 'USA'),
+    Country(name: 'Canada'),
+  ];
+
+  test('should return countries from data source', () async {
+    when(mockDataSource.getAll()).thenAnswer((_) async => testCountries);
+
+    final result = await repository.getAll();
+
+    expect(result, testCountries);
+    verify(mockDataSource.getAll()).called(1);
+  });
+
+  test('should propagate errors from data source', () async {
+    when(mockDataSource.getAll()).thenThrow(Exception('DB error'));
+
+    expect(() => repository.getAll(), throwsException);
+  });
+}
---
+++ test/features/country/domain/usecases/get_all_country_usecase_test.dart
@@ -0,0 +1,32 @@
+
+
+import 'package:flutter_clean_architecture/features/country/domain/entities/country.dart';
+import 'package:flutter_clean_architecture/features/country/domain/usecases/get_all_country_usecase.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+
+@GenerateNiceMocks([MockSpec<CountryRepository>()])
+import 'package:flutter_clean_architecture/features/country/domain/repositories/country_repository.dart';
+import 'package:mockito/mockito.dart';
+
+import 'get_all_country_usecase_test.mocks.dart';
+
+void main() {
+  late MockCountryRepository countryRepository;
+  late GetAllCountryUsecase getAllCountryUsecase;
+
+  setUp(() {
+    countryRepository = MockCountryRepository();
+    getAllCountryUsecase = GetAllCountryUsecase(countryRepository: countryRepository);
+  });
+
+  test("when usecase is called, it should return the list<country>", () async {
+    final countries = [Country(name: 'USA')];
+    when( countryRepository.getAll()).thenAnswer((_) async => countries);
+
+    final result = await getAllCountryUsecase.call();
+    expect(result, countries);
+  });
+
+
+}
